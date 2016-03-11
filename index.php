202860b1133978e00f8800f47a2e850f0f7bf3f7
adrianVaNa <adrian.v.navarrete@gmail.com>
2016-03-10 18:11:31 -0600

    index

---
+++ index.php
@@ -0,0 +1,130 @@
+
+<!DOCTYPE html>
+<html class="no-js" lang="es-mx" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#"  itemscope="" itemtype="http://schema.org/LocalBusiness" xmlns="http://www.w3.org/1999/xhtml" xml:lang="es-mx">
+
+<head>
+	<title>Titulo | slogan</title>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="author" content="Adrian Vazquez">
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="dc.language" content="es_MX"/>
+    <meta property="og:locale" content="es_MX"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+
+	<link rel="shortcut icon"  type="image/x-icon" href="img/favicon.ico">
+	<!--codigo de fuentes-->
+	<link rel="stylesheet" type="text/css" href="css/main.css" > 
+</head>
+<body>
+
+<header id="header" class="header">
+	<menu type="context toolbar">
+		<figure class="menu-logo">
+			<img src="southtvlogo">
+		</figure>
+		<div class="menu-search">
+			<input type="search" placeholder="Busca videos, canales y más...">
+			<button class="purple text-white">S</button>
+		</div>
+		<div class="menu-options purple text-right">
+			<ul>
+				<li><a href="#" class="border-white">¡Registrate ahora!</a></li>
+				<li><a href="#"><img src="login"></a></li>
+				<li><a href="#"><img src="settings"></a></li>	
+			</ul>
+		</div>
+	</menu>
+
+</header>
+
+<section class="tv" id="tv">
+	<div class="column2 left">
+		<iframe src="http://www.filmon.tv/tv/channel/export?channel_id=1672&autoPlay=1&affid=194909Yq" allowfullscreen ></iframe>
+	</div>
+		<div class="column2 right">
+		<table>
+			<tr>
+				<th class="light-gray border-bottom">Lunes 8 de mayo</th><th class="light-gray border-bottom">11:00 a.m.</th>
+				<th class="light-gray border-bottom">12:00 p.m.</th><th class="light-gray border-bottom">13:00 p.m.</th>
+			</tr>
+			<tr>
+				<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
+				<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
+			</tr>
+			<tr>
+				<td class="light-blue"><span>CBC News</span></td><td class="light-gray">Ehicula Imperdiet</td>
+				<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
+			</tr>
+			<tr>
+				<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
+				<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
+			</tr>
+			<tr>
+				<td class="light-blue"><span>CBC News</span></td><td class="light-gray">Ehicula Imperdiet</td>
+				<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
+			</tr>
+			<tr>
+				<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
+				<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
+			</tr>
+			<tr>
+				<td class="light-blue"><span>CBC News</span></td><td class="light-gray">Ehicula Imperdiet</td>
+				<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
+			</tr>
+			<tr>
+				<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
+				<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
+			</tr>
+		</table>
+	</div>
+</section>
+
+<!-- begin footer section -->
+<footer class="footer" id="footer">
+	<div class="cols">
+		<div class="column4 border-right">
+			<p>WEB APP</p>
+			<li><a href="#">Mi tv</a></li>
+			<li><a href="#">Sports Channels</a></li>
+			<li><a href="#">Movie Channels</a></li>
+			<li><a href="#">Lorem Ipsum</a></li>
+			<li><a href="#">Nulla facilis</a></li>
+		</div>
+		<div class="column4 border-right">
+			<p>LOREM IPSUM</p>
+			<li><a href="#">Habitasse</a></li>
+			<li><a href="#">Platea</a></li>
+			<li><a href="#">Dictumst</a></li>
+			<li><a href="#">Proin</a></li>
+			<li><a href="#">Finibus</a></li>
+			<li><a href="#">Sed</a></li>
+			<li><a href="#">Tristique</a></li>
+		</div>
+		<div class="column4 border-right">
+			<p>SOCIAL</p>
+			<li><a href="#">FB-logo</a></li>
+			<li><a href="#">Twitter-folows</a></li>
+		</div>
+		<div class="column4">
+			<p>CONTACTO</p>
+			<p>Nombre (obligatorio)
+				<input type="text">
+				Correo (obligatorio)
+				<input type="mail">
+				Comentario
+				<textarea></textarea>
+			</p>
+		</div>
+	</div>
+	<div class="firma">Todos los derechos reservados a Southboxtv.</div>
+</footer>
+<!-- end footer section -->
+
+</body>
+
+
+
+</body>
+</html>
