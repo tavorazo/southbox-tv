e72e6fa2d70974a670f5627c1aadd1059d0c7dc3
Octavio <octavio.razo.verduzco@gmail.com>
2016-03-14 22:54:42 -0600

    add angular

--- index.php
+++ index.php
@@ -1,91 +1,192 @@
 
 <!DOCTYPE html>
-<html class="no-js" lang="es-mx" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#"  itemscope="" itemtype="http://schema.org/LocalBusiness" xmlns="http://www.w3.org/1999/xhtml" xml:lang="es-mx">
+<html class="no-js" lang="es-mx" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#"  itemscope="" itemtype="http://schema.org/LocalBusiness" xmlns="http://www.w3.org/1999/xhtml" xml:lang="es-mx" ng-app="tv">
 
 <head>
-	<title>Titulo | slogan</title>
+	<title>SouthBox TV | Chile</title>
 	<meta charset="utf-8">
 	<meta name="description" content="">
-	<meta name="author" content="Adrian Vazquez">
+	<meta name="author" content="Octavio Razo & Adrian Vazquez">
 	<meta name="viewport" content="width=device-width">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="dc.language" content="es_MX"/>
     <meta property="og:locale" content="es_MX"/>
     <meta name="apple-mobile-web-app-capable" content="yes"/>
 
-	<link rel="shortcut icon"  type="image/x-icon" href="img/favicon.ico">
+	<link rel="shortcut icon"  type="image/x-icon" href="assets/img/Logo-2.png">
 	<!--codigo de fuentes-->
-	<link rel="stylesheet" type="text/css" href="css/main.css" > 
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css" > 
+
+	<link rel="stylesheet" href="bower_components/slick-carousel/slick/slick.css">
+	<link rel="stylesheet" href="bower_components/slick-carousel/slick/slick-theme.css">
+	<!-- angular -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+	<script src="bower_components/slick-carousel/slick/slick.js"></script>
+	<script src="bower_components/slick-carousel/slick/slick.min.js"></script>
+	<script src="bower_components/angular-slick/dist/slick.js"></script>
+	<script src="bower_components/angular-slick/dist/slick.min.js"></script>
+	<script src="bower_components/tv.js"></script>
+
 </head>
 <body>
 
 <header id="header" class="header">
 	<menu type="context toolbar">
 		<figure class="menu-logo">
-			<img src="southtvlogo">
+			<img src="assets/img/Logo-1.png">
 		</figure>
 		<div class="menu-search">
 			<input type="search" placeholder="Busca videos, canales y más...">
-			<button class="purple text-white">S</button>
+			<button class="purple text-white trans"> 
+			<img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-20.png">
+			</button>
 		</div>
 		<div class="menu-options purple text-right">
 			<ul>
-				<li><a href="#" class="border-white">¡Registrate ahora!</a></li>
-				<li><a href="#"><img src="login"></a></li>
-				<li><a href="#"><img src="settings"></a></li>	
+				<li><a href="#" class="btn2 trans">¡Registrate ahora!</a></li>
+				<li><a href="#"><img src="https://cdn4.iconfinder.com/data/icons/ubercons-web-application/512/Avatar-24.png"></a></li>
+				<li><a href="#"><img src="https://cdn4.iconfinder.com/data/icons/ubercons-web-application/512/Gear-24.png"></a></li>	
 			</ul>
 		</div>
 	</menu>
 
 </header>
 
-<section class="tv" id="tv">
-	<div class="column2 left">
-		<iframe src="http://www.filmon.tv/tv/channel/export?channel_id=299&autoPlay=1"></iframe>
-	</div>
-		<div class="column2 right">
-		<table>
-			<tr>
-				<th class="light-gray border-bottom">Lunes 8 de mayo</th><th class="light-gray border-bottom">11:00 a.m.</th>
-				<th class="light-gray border-bottom">12:00 p.m.</th><th class="light-gray border-bottom">13:00 p.m.</th>
-			</tr>
-			<tr>
-				<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
-				<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
-			</tr>
-			<tr>
-				<td class="light-blue"><span>ESPN</span></td><td class="light-gray">Ehicula Imperdiet</td>
-				<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
-			</tr>
-			<tr>
-				<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
-				<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
-			</tr>
-			<tr>
-				<td class="light-blue"><span>CBC News</span></td><td class="light-gray">Ehicula Imperdiet</td>
-				<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
-			</tr>
-			<tr>
-				<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
-				<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
-			</tr>
-			<tr>
-				<td class="light-blue"><span>CBC News</span></td><td class="light-gray">Ehicula Imperdiet</td>
-				<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
-			</tr>
-			<tr>
-				<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
-				<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
-			</tr>
-		</table>
-	</div>
-</section>
+<main >
+	<section class="tv" id="tv" >
+		<div class="column2 left">
+			<iframe src="http://www.filmon.tv/tv/channel/export?channel_id=299&autoPlay=1"></iframe>
+		</div>
+			<div class="column2 right">
+			<table>
+				<tr>
+					<th class="light-gray border-bottom">Lunes 8 de mayo</th><th class="light-gray border-bottom">11:00 a.m.</th>
+					<th class="light-gray border-bottom">12:00 p.m.</th><th class="light-gray border-bottom">13:00 p.m.</th>
+				</tr>
+				<tr>
+					<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
+					<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
+				</tr>
+				<tr>
+					<td class="light-blue"><span>ESPN</span></td><td class="light-gray">Ehicula Imperdiet</td>
+					<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
+				</tr>
+				<tr>
+					<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
+					<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
+				</tr>
+				<tr>
+					<td class="light-blue"><span>CBC News</span></td><td class="light-gray">Ehicula Imperdiet</td>
+					<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
+				</tr>
+				<tr>
+					<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
+					<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
+				</tr>
+				<tr>
+					<td class="light-blue"><span>CBC News</span></td><td class="light-gray">Ehicula Imperdiet</td>
+					<td class="light-blue">Donec Diam Risus</td><td class="light-gray">Ehicula Imperdiet</td>
+				</tr>
+				<tr>
+					<td class="light-gray"><span>CBC News</span></td><td class="light-blue">Ehicula Imperdiet</td>
+					<td class="light-gray">Donec Diam Risus</td><td class="light-blue">Ehicula Imperdiet</td>
+				</tr>
+			</table>
+		</div>
+	</section>	
+
+	<section id="corousel">
+		<div class="container">
+			<h2>Mi TV</h2>
+			<slick infinite="true" slides-to-show="4" slides-to-scroll="1">
+	  			<div class="channel">
+	  				<img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTZFGBdSECwzao4VtbPpgvZmi5a_SRbv25Yj41VbUn-k9qYFHoE" alt="">
+	  			</div>
+	 			<div class="channel">
+	  				<img src="" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="" alt="">
+	  			</div>
+			</slick>
+		</div>
+	</section>	
+	<section id="corousel">
+		<div class="container">
+			<h2>Canales Deportivos</h2>
+			<slick infinite="true" slides-to-show="4" slides-to-scroll="1">
+	  			<div class="channel">
+	  				<img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTZFGBdSECwzao4VtbPpgvZmi5a_SRbv25Yj41VbUn-k9qYFHoE" alt="">
+	  			</div>
+	 			<div class="channel">
+	  				<img src="http://www.wonderslist.com/wp-content/uploads/2013/11/ESPN-logo.jpg" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="http://relhostpic.eu/images/2014/11/26/TF3Kv.jpg" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="http://www.wonderslist.com/wp-content/uploads/2013/11/ESPN-logo.jpg" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="http://relhostpic.eu/images/2014/11/26/TF3Kv.jpg" alt="">
+	  			</div>
+			</slick>
+		</div>
+	</section>	
+	<section id="corousel">
+		<div class="container">
+			<h2>Canales de peliculas</h2>
+			<slick infinite="true" slides-to-show="4" slides-to-scroll="1">
+	  			
+	 			<div class="channel">
+	  				<img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Hallmark_Movie_Channel.svg/800px-Hallmark_Movie_Channel.svg.png" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="http://cdn.24.co.za/files/Cms/General/d/1546/660448aa6b5e4b0dbb5ba6b37380e4ae.jpg" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="http://www.blackfilm.com/read/wp-content/uploads/2014/12/Urban-Movie-Channel-logo.jpg" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQEzes0Xy4U0xBQMW9kF5_XqWJkTy0h_5Ht9QrSFNZXapteVx8M" alt="">
+	  			</div>
+	  			<div class="channel">
+	  				<img src="http://cdn2-b.examiner.com/sites/default/files/styles/image_content_width/hash/3b/75/3b75b9ee0d309c224fdac377ed6bfb2d.png?itok=UH4-D51Z" alt="">
+	  			</div>
+			</slick>
+		</div>
+	</section>	
+	<section id="download" class="light-gray">
+		<div class="container">
+			<div class="column4"></div>
+			<div class="column4">
+				<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSN74Bkrry4nMYYfdbR-lfMV-p8MOM8cykmEnyjYQe7SaQvZUXc" alt="">
+			</div>
+			<div class="column4">
+				<img src="https://buffer.com/images/common/app-store.png" alt="">
+			</div>
+			<div class="column4"></div>
+		</div>
+	</section>
+
+</main>
+
+
+
+
 
 <!-- begin footer section -->
 <footer class="footer" id="footer">
 	<div class="cols">
 		<div class="column4 border-right">
-			<p>WEB APP</p>
+			<p class="strong">WEB APP</p>
 			<li><a href="#">Mi tv</a></li>
 			<li><a href="#">Sports Channels</a></li>
 			<li><a href="#">Movie Channels</a></li>
@@ -93,7 +194,7 @@
 			<li><a href="#">Nulla facilis</a></li>
 		</div>
 		<div class="column4 border-right">
-			<p>LOREM IPSUM</p>
+			<p class="strong">LOREM IPSUM</p>
 			<li><a href="#">Habitasse</a></li>
 			<li><a href="#">Platea</a></li>
 			<li><a href="#">Dictumst</a></li>
@@ -103,12 +204,12 @@
 			<li><a href="#">Tristique</a></li>
 		</div>
 		<div class="column4 border-right">
-			<p>SOCIAL</p>
+			<p class="strong">SOCIAL</p>
 			<li><a href="#">FB-logo</a></li>
 			<li><a href="#">Twitter-folows</a></li>
 		</div>
 		<div class="column4">
-			<p>CONTACTO</p>
+			<p class="strong">CONTACTO</p>
 			<p>Nombre (obligatorio)
 				<input type="text">
 				Correo (obligatorio)
@@ -118,7 +219,7 @@
 			</p>
 		</div>
 	</div>
-	<div class="firma">Todos los derechos reservados a Southboxtv.</div>
+	<div class="firma"><p>Todos los derechos reservados  <a href="#" title="">Southboxtv</a>. | Created by <a href="htp://tavorazo.github.io" title="">Octavio Razo</a></p></div>
 </footer>
 <!-- end footer section -->
 
